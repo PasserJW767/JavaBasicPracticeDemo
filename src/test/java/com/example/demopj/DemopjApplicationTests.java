f6bcee7582914423ec8ac9ba08b4aa1fc4eac215
LiutongJiang <709263897@qq.com>
2024-04-30 20:36:34 +0800

    Initial commit

---
+++ src/test/java/com/example/demopj/DemopjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
