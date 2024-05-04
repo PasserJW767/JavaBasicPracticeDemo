3d67c9321928db9024fbc4f57b6cbc5f5e11ecd5
PasserJW767 <709263897@qq.com>
2024-05-04 21:36:55 +0800

    Ali OSS Content

---
+++ src/test/java/com/example/demopj/AliOSSTest.java
@@ -0,0 +1,49 @@
+package com.example.demopj;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.io.FileInputStream;
+
+@SpringBootTest
+public class AliOSSTest {
+
+    @Test
+    public void test() throws Exception {
+        // Endpoint修改成了深圳的节点
+        String endpoint = "https://oss-cn-shenzhen.aliyuncs.com";
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+//        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "an-test-bucket";
+        // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+        String objectName = "pic1.png";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+
+        try {
+            String content = "Hello OSS";
+            ossClient.putObject(bucketName, objectName, new FileInputStream("D:\\桌面\\SpringBoot与Vue开发知识\\SpringBoot\\pic1.png"));
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+}
