54f8275427c2ad24f80a82de0fff297715ba4567
LiutongJiang <709263897@qq.com>
2024-05-16 09:50:21 +0800

    Zookeeper: Curator - update and delete

--- src/test/java/com/example/demopj/Curator.java
+++ src/test/java/com/example/demopj/Curator.java
@@ -3,6 +3,8 @@ package com.example.demopj;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.BackgroundCallback;
+import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
@@ -37,6 +39,8 @@ public class Curator {
                 .retryPolicy(retryPolicy).build();
         client.start();
     }
+
+//    ==========================================================create==================================================
 
     /*
      * 创建节点 create 持久 临时 顺序 数据
@@ -73,7 +77,7 @@ public class Curator {
         System.out.println(path);
     }
 
-
+//    ==========================================================get=====================================================
     /*
      * 查询节点 get: getData().forPath()
      * 1. 查询数据: get(): getData().forPath(..)
@@ -102,6 +106,63 @@ public class Curator {
         System.out.println(stat);
         System.out.println(new String(bytes));
     }
+
+//    ==========================================================update==================================================
+    /*
+     * 修改节点
+     * 1. 基本修改数据: setData().forPath(...)
+     * 2. 根据版本修改: setData().withVersion(...).forPath(...)
+     *      version是通过查询获得的，目的是为了让其他客户端或者线程不干扰
+     */
+    @Test
+    public void testSet() throws Exception{
+        client.setData().forPath("/app1", "lala".getBytes());
+    }
+
+    @Test
+    public void testSetWithVesion() throws Exception{
+        Stat stat = new Stat();
+        client.getData().storingStatIn(stat).forPath("/app1");
+        int version = stat.getVersion();
+        client.setData().withVersion(version).forPath("/app1", "wen".getBytes());
+    }
+
+//    ==========================================================delete==================================================
+    /*
+     * 删除节点
+     * 1. 删除单个节点: delete().forPath(...)
+     * 2. 删除带有子节点的节点: delete().deletingchildrenIfNeeded().forPath(...);
+     * 3. 必须成功的删除: 为了防止网络抖动。本质就是重试。 client.delete().guaranteed().forPath(...);
+     * 4. 回调: inBackground
+     */
+    @Test
+    public void delete1() throws Exception{
+        client.delete().forPath("/app1");
+    }
+
+    @Test
+    public void delete2() throws Exception{
+        client.delete().deletingChildrenIfNeeded().forPath("/app4");
+    }
+
+    @Test
+    public void delete3() throws Exception{
+        client.delete().guaranteed().forPath("/app2");
+    }
+
+    @Test
+    public void delet4() throws Exception{
+        client.delete().inBackground(new BackgroundCallback() {
+            @Override
+            public void processResult(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
+                System.out.println("成功删除");
+                System.out.println(curatorEvent);
+                System.out.println("===============分割线==================");
+                System.out.println(curatorFramework);
+            }
+        }).forPath("/app1");
+    }
+
 
     @AfterEach
     public void closeConnect(){
